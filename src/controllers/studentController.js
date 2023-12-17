1c9b92d49ec95d9bd7d6049900646a66dee75476
vasotodorovic <vaso@dynato.com>
2023-12-17 22:50:44 +0100

    add POST method, need to resolve id autogenerate, a assigned it to psql

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const studentService = require('../services/studentService');
-
+router.use(express.json());
 /**
  * @swagger
  * /students:
@@ -12,7 +12,28 @@ const studentService = require('../services/studentService');
  *     responses:
  *       200:
  *         description: Returns all students
+ *   post:
+ *     summary: Create a new student
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               grade:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Student created successfully
+ *       400:
+ *         description: Invalid request data
  */
+
 router.get('/students', async (req, res) => {
   try {
     const students = await studentService.getAllStudents();
@@ -21,6 +42,16 @@ router.get('/students', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+router.post('/students', async (req, res) => {
+  try {
+    // Call the service method to create a new student
+    const newStudent = await studentService.createStudent(req.body);
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 module.exports = router;
 
--- src/services/studentService.js
+++ src/services/studentService.js
@@ -10,6 +10,15 @@ class StudentService {
       throw new Error('Unable to fetch students');
     }
   }
+  async createStudent(studentDto) {
+    try {
+      // Create the student in the database
+      const newStudent = await Student.create(studentDto);
+      return newStudent;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 module.exports = new StudentService();
